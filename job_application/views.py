993fd225de1f6d7534e69a236fe4c1037133cc06
jose rasgado <jrasgado01@hotmail.com>
2024-09-25 19:21:36 -0600

    Created an application form class inside forms.py, then imported that application form class into views, validated and extracted the user values from the form instance using the cleaned_data dictionary

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .forms import ApplicationForm #the dot at the beginning is because we want to import from the same directory, not the root directory
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"] # dictionary created by Django which pics the names from the HTML
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            print(first_name)
     return render(request, "index.html")
